06fe625fe7cab4c508cfcddfaf3a2e80967f3b9f
hziulk <mizuho.matsuwaka@gmail.com>
2024-11-22 13:41:01 +0900

    いっちゃん最初

---
+++ FpsControll.h
@@ -0,0 +1,15 @@
+//#pragma once
+#include "DxLib.h"
+
+#ifndef FPSCONTROLL_H
+#define FPSCONTROLL_H
+
+void FpsControll_Initialize();
+
+bool FpsControll_Update();
+
+void FpsControll_Draw();
+
+void FpsControll_Wait();
+
+#endif
